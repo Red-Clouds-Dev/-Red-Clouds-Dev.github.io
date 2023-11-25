7336ec3884852caf68f4d32312dedb5625512640
Tristan Jordan <tristan.jordan@live.com>
2023-11-25 18:40:35 -0500

    Made it so when its dark theme it says dark
    had to add to my script.js so when it goes to a dark theme it stays dark till you change it back

--- Tristan/script.js
+++ Tristan/script.js
@@ -1,3 +1,13 @@
 document.getElementById('themeToggleBtn').addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme');
-  });
+  document.body.classList.toggle('dark-theme');
+  const isDarkTheme = document.body.classList.contains('dark-theme');
+  localStorage.setItem('darkTheme', isDarkTheme);
+});
+
+// Check for stored theme preference on page load
+document.addEventListener('DOMContentLoaded', function() {
+  const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
+  if (isDarkTheme) {
+    document.body.classList.add('dark-theme');
+  }
+});
